1f9221383452ea1aacdbda07392852669e88aa2d
Jack Cook <hello@jackcook.com>
2020-08-31 01:11:36 -0600

    Got video chatting to work in the React app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { PionEvents, PionSession } from "pion-browser-client";
+import adapter from "webrtc-adapter";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const SIGNALER_URI = "localhost:5001";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sources: [],
+    };
+
+    navigator.getUserMedia(
+      {
+        video: true,
+        audio: true,
+      },
+      (localMediaStream) => {
+        let localVideo = document.getElementById("localVideo");
+        localVideo.srcObject = localMediaStream;
+        localVideo.onloadedmetadata = function (e) {
+          localVideo.play();
+        };
+
+        let pionSession = new PionSession(SIGNALER_URI, "", {
+          iceServers: [
+            {
+              urls: "stun:localhost",
+            },
+          ],
+          mandatory: { OfferToReceiveVideo: true, OfferToReceiveAudio: true },
+        });
+
+        pionSession.eventHandler = (event) => {
+          switch (event.type) {
+            case PionEvents.MEDIA_START:
+              this.setState({
+                sources: this.state.sources.concat([event.media]),
+              });
+
+              break;
+            case PionEvents.MEDIA_STOP:
+              const idx = this.state.sources.find(
+                (media) => event.media.id === media.id
+              );
+
+              const sources = this.state.sources;
+              sources.splice(idx, 1);
+
+              this.setState({
+                sources,
+              });
+
+              break;
+            default:
+              console.warn(event);
+          }
+        };
+        pionSession.start();
+        pionSession.addMedia(localMediaStream);
+      },
+      function (err) {
+        console.log(
+          "The following error occurred when trying to use getUserMedia: " + err
+        );
+      }
+    );
+  }
+
+  componentDidUpdate() {
+    this.state.sources.forEach((media) => {
+      const videoElem = document.getElementById(media.id);
+      videoElem.srcObject = media;
+      videoElem.onloadedmetadata = function (e) {
+        videoElem.play();
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <video id="localVideo" controls muted />
+        <div id="remoteVideos">
+          {this.state.sources.map((media) => (
+            <video id={media.id} controls />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
